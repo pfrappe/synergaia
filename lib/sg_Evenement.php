1cd79569ea9ec0707e80e6cfbb0ea8ddc9c17f6a
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 14:37:48 +0200

    SynerGaia 2.6

--- lib/sg_Evenement.php
+++ lib/sg_Evenement.php
@@ -1,23 +1,35 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.1.1 (see AUTHORS file)
-* Classe SynerGaia de traitement des événements d'agenda
-*/
-// 2.1.1 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+<?php
+/** SYNERGAIA fichier pour le traitement de l'objet @Evenement */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
+
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_Evenement_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_Evenement_trait.php';
 } else {
+	/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur */
 	trait SG_Evenement_trait{};
 }
+
+/**
+ * Classe SynerGaia de traitement des événements d'agenda
+ * @since 1.1
+ * @version 2.1.1
+ */
 class SG_Evenement extends SG_Document {
-	// Type SynerGaia
+	/** string Type SynerGaia '@Evenement' */
 	const TYPESG = '@Evenement';
+	/** string code de la base des événements */
+	const CODEBASE = 'synergaia_evenements';
+
+	/** string Type SynerGaia */
 	public $typeSG = self::TYPESG;
 	
-	const CODEBASE = 'synergaia_evenements';
-	
 	/** 1.1 ajout
-	* Construction de l'évènement
-	*/
+	 * Construction de l'évènement
+	 * @since 1.1 ajout
+	 * @param string|SG_Texte|SG_Formule $pCodeDocument
+	 * @param null|array $pTableau tableau des propriété
+	 */
 	public function __construct ($pCodeDocument = '', $pTableau = null) {
 		$tmpCode = new SG_Texte($pCodeDocument);
 		$base = SG_Dictionnaire::getCodeBase($this -> typeSG);
@@ -29,11 +41,14 @@ class SG_Evenement extends SG_Document {
 		$this -> code = $this -> getValeur('@Code', '');
 		$this -> setValeur('@Type', '@DictionnaireMethode');
 	}
-	/** 1.1
-	* déplace un événément d'une certaine quantité de temps
-	* @param $pQuantite @Nombre quantité du déplacement (peut-être négatif)
-	* @param $pUnite @Texte ('heure', 'jour' , 'mois', 'année')
-	*/
+
+	/**
+	 * Déplace un événément d'une certaine quantité de temps
+	 * @since 1.1
+	 * @param nombre|SG_Nombre|SG_Formule $pQuantite quantité du déplacement (peut-être négatif)
+	 * @param string|SG_Texte|SG_Formule $pUnite 'heure', 'jour' , 'mois', 'année'
+	 * @return SG_Evenement l'événement en cours
+	 */
 	function DeplacerDe ($pQuantite = 0, $pUnite = '') {
 		$qte = new SG_Nombre($pQuantite);
 		$qte = $qte -> valeur;
@@ -49,11 +64,13 @@ class SG_Evenement extends SG_Document {
 		}
 		return $this;	
 	}
-	/** 1.1
-	* déplace un événément d'une certaine quantité de temps
-	* @param $pQuantite @Nombre quantité du déplacement (peut-être négatif)
-	* @param $pUnite @Texte ('heure', 'jour' , 'mois', 'année')
-	*/
+
+	/**
+	 * déplace un événément d'une certaine quantité de temps
+	 * @since 1.1
+	 * @param SG_DateHeure|SG_Date|SG_Formule $pDateHeure date (et heure) du déplacement
+	 * @return SG_Evenement l'événement en cours
+	 */
 	function DeplacerAu ($pDateHeure = '' ) {
 		if ($pDateHeure !== '') {
 			$dt = new SG_DateHeure($pDateHeure);
@@ -66,10 +83,13 @@ class SG_Evenement extends SG_Document {
 			return $this;
 		}	
 	}
-	/** 1.1 ajout
-	* Durée de l'événement dans l'unité fournie
-	* @param $pUnite @Texte unité de durée
-	*/
+
+	/**
+	 * Durée de l'événement dans l'unité fournie
+	 * @since 1.1 ajout
+	 * @param string|SG_Texte|SG_Formule $pUnite unité de la durée
+	 * @return SG_Nombre
+	 */
 	function Duree ($pUnite = 'heure') {
 		$ret = 0;
 		$unite = new SG_Texte($pUnite);
@@ -83,6 +103,7 @@ class SG_Evenement extends SG_Document {
 		}
 		return new SG_Nombre($duree);		
 	}
+
 	// 2.1.1. complément de classe créée par compilation
 	use SG_Evenement_trait;
 }
