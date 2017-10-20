6a08963f1078ce66bc6b3c76fce9b91bff614ff6
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 14:39:37 +0200

    SynerGaia 2.6

--- lib/sg_Formulaire.php
+++ lib/sg_Formulaire.php
@@ -1,27 +1,37 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.1.1 (see AUTHORS file)
-* Classe SynerGaia de traitement des textes riches avancés
-*/
-// 2.1.1 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+<?php
+/** SYNERGAIA fichier pour le traitement de l'objet @Formulaire */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_Formulaire_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_Formulaire_trait.php';
 } else {
+	/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+	 * @since 2.1.1
+	 */
 	trait SG_Formulaire_trait{};
 }
+
+/**
+ * Classe SynerGaia de traitement des textes riches avancés
+ * @version 2.1.1
+ */
 class SG_Formulaire extends SG_Document {
-	// Type SynerGaia
+	/** string Type SynerGaia '@Formulaire' */
 	const TYPESG = '@Formulaire';
+
+	/** string Type d'objet SynerGaïa */
 	public $typeSG = self::TYPESG;
-	
-	// Code de la base
+
+	/** string Code de la base */
 	const CODEBASE = 'synergaia_formulaires';
 
-	/** 1.3.1
-	* Construction de l'objet
-	*
-	* @param string $pCode code du theme
-	* @param array $pTableau tableau éventuel des propriétés
-	*/
+	/**
+	 * Construction de l'objet
+	 *
+	 * @since 1.3.1
+	 * @param string $pCode code du theme
+	 * @param array $pTableau tableau éventuel des propriétés
+	 */
 	public function __construct($pCode = '', $pTableau = null) {
 		$tmpCode = new SG_Texte($pCode);
 		$base = SG_Dictionnaire::getCodeBase($this -> typeSG);
@@ -33,13 +43,17 @@ class SG_Formulaire extends SG_Document {
 		$this -> code = $this -> getValeur('@Code');
 		$this -> setValeur('@Type', self::TYPESG);
 	}
-	/** 1.3.1
-	* Affichage du texte paramétré dans lequel des formules SynerGaïa sont intégrées entre {{formula}}
-	*
-	* @return string code HTML
-	*/
+
+	/**
+	 * Affichage du texte paramétré dans lequel des formules SynerGaïa sont intégrées entre {{formula}}
+	 *
+	 * @since 1.3.1
+	 * @version 2.6 rerour SG_HTML
+	 * @param string|SG_Texte|SG_Formule $pSurObjet
+	 * @return SG_HTML code HTML
+	 */
 	function Afficher($pSurObjet = '') {
-		$ret = '';
+		$ret = new SG_HTML();
 		if($pSurObjet !== '') {
 			if(getTypeSG($pSurObjet) === '@Formule') {
 				$principal = $pSurObjet -> calculer();
@@ -59,6 +73,7 @@ class SG_Formulaire extends SG_Document {
 		}
 		return $ret;
 	}
+
 	// 2.1.1. complément de classe créée par compilation
 	use SG_Formulaire_trait;
 }
