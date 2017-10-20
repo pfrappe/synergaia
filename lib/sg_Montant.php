87f2aeca230b557d73641ba9b6994fb81e63fadb
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 14:59:54 +0200

    SynerGaia 2.6

--- lib/sg_Montant.php
+++ lib/sg_Montant.php
@@ -1,18 +1,28 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.1.1 (see AUTHORS file)
-* Classe SynerGaia de gestion d'un montant (monétaire)
-*/
-// 2.1.1 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+<?php
+/** SYNERGAIA fichier pour le traitement de l'objet @Montant */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_Montant_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_Montant_trait.php';
 } else {
+	/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+	 * @since 2.1.1
+	 */
 	trait SG_Montant_trait{};
 }
+
+/**
+ * Classe SynerGaia de gestion d'un montant (monétaire)
+ * @version 2.1.1
+ * @todo : à terminer ou remplacer par @Nombre avec @Unite
+ */
 class SG_Montant extends SG_Nombre {
-    // Type SynerGaia
-    const TYPESG = '@Montant';
-    // Type SynerGaia de l'objet
-    public $typeSG = self::TYPESG;
+	/** string Type SynerGaia '@Montant' */
+	const TYPESG = '@Montant';
+
+	/** Type SynerGaia de l'objet */
+	public $typeSG = self::TYPESG;
+
 	// 2.1.1. complément de classe créée par compilation
 	use SG_Montant_trait;
 }
