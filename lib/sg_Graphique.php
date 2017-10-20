c482b70303a8ca6e8fd937ba8a33745f75415579
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 14:44:59 +0200

    SynerGaia 2.6

--- lib/sg_Graphique.php
+++ lib/sg_Graphique.php
@@ -1,16 +1,26 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.1.1 (see AUTHORS file)
-* Classe SynerGaia représentant un graphique à afficher
-*/
-// 2.1.1 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+<?php
+/** SYNERGAIA fichier pour le traitement de l'objet @Graphique
+ * @todo voir si utilisé ?? */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_Graphique_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_Graphique_trait.php';
 } else {
+	/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+	 * @since 2.1.1
+	 */
 	trait SG_Graphique_trait{};
 }
+
+/**
+ * Classe SynerGaia représentant un graphique à afficher
+ * L'objectif de cette classe est d'y accrocher les code javascript pour l'animation
+ * @version 2.1.1
+ */
 class SG_Graphique extends SG_HTML {
-	// Type SynerGaia
+	/** string Type SynerGaia '@Graphique' */
 	const TYPESG = '@Graphique';
+	/** string Type SynerGaia */
 	public $typeSG = self::TYPESG;
 
 	/** 2.0
