5750fd12401b6918d7fe8e6a8373eb213fa66f41
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 13:57:52 +0200

    SynerGaia 2.6

--- lib/sg_BaseDominoDB.php
+++ lib/sg_BaseDominoDB.php
@@ -1,20 +1,43 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 1.3.4 (see AUTHORS file)
-* 
-* SG_BaseDominoDB : Classe de gestion d'une base de documents Domino
-*
-*/
+<?php
+/** SYNERGAIA fichier pour le traitement de l'objet @BaseDominoDB */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
+/**
+ * SG_BaseDominoDB : Classe de gestion d'une base de documents Domino
+ * @since 1.3.4 (see AUTHORS file)
+ * @todo obsolète ? ou inutile ?
+ */
 class SG_BaseDominoDB extends SG_Objet {
-	// Type SynerGaia
+	/** string Type SynerGaia */
 	const TYPESG = '@BaseDominoDB';
+	/** string Type SynerGaia */
 	public $typeSG = self::TYPESG;
-	
+
+	/** string cookie domino */
 	public $cookie = '';
+	/** string utlilisateur Lotus Notes */
 	public $user = '';
+	/** string */
 	public $psw = '';
+	/** string */
 	public $serveur = '';
-	
+
+	/**
+	 * Constuction de l'objet (l'accès à Domino se fait par les services WEB)
+	 * @since 1.3.4
+	 * @param string|SG_Texte|Formule $pServeur
+	 * @param string|SG_Texte|Formule $pUser
+	 * @param string|SG_Texte|Formule $pPassword
+	 */
 	public function __construct($pServeur = '', $pUser = '', $pPassword = '') {
+	}
+
+	/**
+	 * Compacter la base (inutilisé)
+	 * @since 2.4 ajout pour compatibilité avec bases CouchDB
+	 */
+	function Compacter() {
+		return new SG_VraiFaux(true);
 	}
 }
 ?>
