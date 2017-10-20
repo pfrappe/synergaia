b540245bde53edfe2e56692c54b9ef69801fefc7
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 14:35:00 +0200

    SynerGaia 2.6

--- lib/sg_Dossier.php
+++ lib/sg_Dossier.php
@@ -1,24 +1,34 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.3 (see AUTHORS file)
-* Classe SynerGaia de traitement des dossiers dans un document
-*/
-// Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur, via un trait à la fin de la classe
+<?php
+/** SYNERGAIA fichier pour le traitement del'objet @Dossier
+ * @todo à conserver ??
+ */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_Dossier_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_Dossier_trait.php';
 } else {
+	/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur, via un trait à la fin de la classe */
 	trait SG_Dossier_trait{};
 }
+
+/**
+ * Classe SynerGaia de traitement des dossiers dans un document
+ * @version 2.3
+ */
 class SG_Dossier extends SG_Objet {
-	// Type SynerGaia
+	/** string Type SynerGaia '@Dossier' */
 	const TYPESG = '@Dossier';
+	/** string Type SynerGaia */
 	public $typeSG = self::TYPESG;
 	
-	// élements du dossier (fichiers internes, renvois vers des @Document, @Dossier = sous-dossiers)
-	// chaque élément est un tableau :[type, titre, datemodification, id ou clé]
+	/** @var élements du dossier (fichiers internes, renvois vers des @Document, @Dossier = sous-dossiers)
+	 * chaque élément est un tableau :[type, titre, datemodification, id ou clé]
+	 */
 	public $elements = array();
 	
 	/**
 	* Construction de l'objet
+	* @param any $pQuelqueChose
 	*/
 	function __construct($pQuelqueChose = null) {
 	}
