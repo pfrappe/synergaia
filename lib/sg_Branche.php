6d282255ea64429a01892d1063200bb4ecb54fad
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 14:00:53 +0200

    SynerGaia 2.6 (unused class)

--- lib/sg_Branche.php
+++ lib/sg_Branche.php
@@ -1,37 +1,55 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.2 (see AUTHORS file) : INUTILISE ACTUELLEMENT
-* Classe SynerGaia représentant une branche php d'une phrase SynerGaïa
-**/
+<?php
+/** SYNERGAIA fichier pour le traitement de l'objet @Branche */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
+/** INUTILISE ACTUELLEMENT
+ * Classe SynerGaia représentant une branche php d'une phrase SynerGaïa
+ * @since 2.1
+ */
 class SG_Branche extends SG_Objet{
-	// Type SynerGaia
+	/** string Type SynerGaia '@Branche' */
 	const TYPESG = '@Branche';
+	/** string Type SynerGaia */
 	public $typeSG = self::TYPESG;
-	
+
+	/** string  */
 	public $texte = '';
+	/** string  */
 	public $php='';
+	/** string  */
 	public $index;
+	/** string Opération appelante */
 	public $operation;
+	/** string Etape en cours d'exécution */
 	public $etape;
+	/** string Objet principal actuel pour cette étape de cette branche */
 	public $objet;
+	/** string Résultat général en cours de constitution */
 	public $resultat;
+	/** string  */
 	public $etiquette;
+	/** string Code étape suivante de la branche */
 	public $suite;
 	
-	/** 2.1 ajout
-	* Construction de l'objet
-	* @param (string ou objet) $pPHP : texte à exécuter
-	* @param indéfini $pIndex : n° de la branche dans la liste des branches
-	*/
+	/**
+	 * Construction de l'objet
+	 * @since 2.1 ajout
+	 * @param indéfini $pIndex : n° de la branche dans la liste des branches
+	 * @param string|SG_Texte|SG_Formule $pPHP : texte à exécuter
+	 * @param string|SG_Texte|SG_Formule $pTexte : texte à exécuter
+	 */
 	public function __construct($pIndex = 0, $pPHP = '', $pTexte = '') {
 		$this -> php = SG_Texte::getTexte($pPHP);
 		$this -> index = $pIndex;
 		$this -> texte = $pTexte;
 	}
-	/** 2.1 ajout
-	* Exécute le texte PHP avec les paramètres fournis
-	* @param (SG_Objet) $pObjet : objet sur lequel porte la branche. Par défaut SG_Rien
-	* @param (SG_Operation) $pOperation : opération en cours dans laquelle se trouvent les variables et le tableau des branches
-	**/
+	/**
+	 * Exécute le texte PHP avec les paramètres fournis
+	 * @since 2.1 ajout
+	 * @param SG_Objet $pObjet : objet sur lequel porte la branche. Par défaut SG_Rien
+	 * @param SG_Operation $pOperation : opération en cours dans laquelle se trouvent les variables et le tableau des branches
+	 * @return SG_Obet|SG_Erreur
+	 */
 	public function Executer($pObjet, $pOperation) {
 		$ret = false;
 		$objet = $pObjet;
@@ -44,18 +62,24 @@ class SG_Branche extends SG_Objet{
 		}
 		return $resultat;
 	}
-	/** 2.1 ajout
-	* Crée un bouton submit avec le texte proposé et renvoie vers l'étape passée en paramètre
-	* @param (string) $pTexte : texte du bouton
-	* @param (integer) $pNoBranche : n° de la branche à initialiser au retour (paramètre e= de l'url)
-	**/
+
+	/**
+	 * Crée un bouton submit avec le texte proposé et renvoie vers l'étape passée en paramètre
+	 * @since 2.1 ajout
+	 * @param (string) $pTexte : texte du bouton
+	 * @param (integer) $pNoBranche : n° de la branche à initialiser au retour (paramètre e= de l'url)
+	 * @return string
+	 */
 	public function Submit($pTexte = '', $pNoBranche = 0) {
 		$ret = '';
 		return $ret;
 	}
+
 	/** 2.1 ajout
-	* Affiche en html le texte php
-	**/
+	 * Affiche en html le texte php
+	 * @since 2.1 ajout
+	 * @return SG_HTML
+	 */
 	public function Afficher() {
 		return new SG_HTML($this -> php);
 	}
