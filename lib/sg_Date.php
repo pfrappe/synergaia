7dbe437b35318d2c15eb90b08d62925ba5472121
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 14:19:57 +0200

    SynerGaia 2.6

--- lib/sg_Date.php
+++ lib/sg_Date.php
@@ -1,97 +1,95 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.3 (see AUTHORS file)
-* SG_Date : Classe de traitement des dates
-*/
-// 2.1.1 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+<?php
+/** sg_Date.php SynerGaia contient la classe SG_Date de traitement des dates */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
+/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur */
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_Date_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_Date_trait.php';
 } else {
+	/** trait vide par défaut */
 	trait SG_Date_trait{};
 }
+
+/** SynerGaia SG_Date : Classe de traitement des dates
+ * @version 2.6.0
+ * @since 0.0
+ */
 class SG_Date extends SG_Objet {
-	// Type SynerGaia
+
+	/** string Type SynerGaia */
 	const TYPESG = '@Date';
-	// Type SynerGaia de l'objet
+
+	/** string Type SynerGaia de l'objet */
 	public $typeSG = self::TYPESG;
 
 	/**
 	 * Valeur interne de la date
+	 * @version 2.6 c'est un DateTime
+	 * @var DateTime
 	 */
 	public $_date;
 
-	/** 1.1 : traite DateTime ; 1.3.0 correction ; 2.3 si @DateHeure
+	/** 
 	 * Construction de l'objet
-	 *
+	 * @version 2.6 instanceof, replace '-'
 	 * @param indéfini $pQuelqueChose valeur à partir de laquelle créer la date
 	 */
 	public function __construct($pQuelqueChose = null) {
-		if (!is_null($pQuelqueChose)) {
-			$tmpTypeSG = getTypeSG($pQuelqueChose);
-
-			switch ($tmpTypeSG) {
-				case 'integer' :
-				case 'double' :
-					$this -> _date = strtotime(date('d-m-Y', $pQuelqueChose));
-					break;
-				case 'string' :
-					// Lit la date passée
-					$elements = explode('/', $pQuelqueChose);
-					if (sizeof($elements) === 3) {
-						$jour = $elements[0];
-						$mois = $elements[1];
-						$annee = $elements[2];
-						if (is_numeric($jour)) {
-							if (is_numeric($mois)) {
-								if (is_numeric($annee)) {
-									$this -> _date = mktime(0, 0, 0, $mois, $jour, $annee);
-								}
-							}
+		if ($pQuelqueChose instanceof SG_Formule) {
+			$prm = $pQuelqueChose -> calculer();
+		} else {
+			$prm = $pQuelqueChose;
+		}
+		if (!is_null($prm)) {
+			if ($prm instanceof SG_Date) {
+				$this -> _date = $prm -> _date;
+			} elseif ($prm instanceof SG_DateHeure) {
+				$this -> _date = $prm -> _instant;
+				$this -> _date -> setTime(0,0);
+			} else {
+				if (is_object($prm) and $prm instanceof DateTime) {
+					$this -> _date = $prm;
+					$this -> _date -> setTime(0,0);
+				} else {
+					$d = SG_Texte::getTexte($prm);
+					if ($d !== '') {
+						$d = trim(str_replace('/', '-', $d));
+						if (strstr($d, ' ') !== false) {
+							$d = substr($d, strpos($d, ' '));
+						}
+						try {
+							$this -> _date = new DateTime($d);
+						} catch (Exception $e){
+							journaliser('Erreur de date :' . $d);
 						}
 					}
-					break;
-				case 'object' :
-					if(get_class($pQuelqueChose) === 'DateTime') {
-						$this -> _date = $pQuelqueChose -> getTimestamp();
-					}
-					break;
-				case '@Date' :
-					$this -> _date = $pQuelqueChose -> _date;
-					break;
-				case '@DateHeure' :
-					$d = $pQuelqueChose -> _instant;
-					$this -> _date = mktime(0, 0, 0, date('m', $d)  , date('d', $d), date('Y', $d));
-					break;
-				default :
-					if (substr($tmpTypeSG, 0, 1) === "@") {
-						// Si objet SynerGaia
-						if ($tmpTypeSG === '@Formule') {
-							$tmp = $pQuelqueChose -> calculer();
-							$tmpDate = new SG_Date($tmp);
-							$this -> _date = $tmpDate -> _date;
-						} else {
-							$tmpDate = new SG_Date($pQuelqueChose -> toString());
-							$this -> _date = $tmpDate -> _date;
-						}
-					}
+				}
 			}
 		}
 	}
 
 	/**
 	 * Renvoie le timestamp unix de l'objet
-	 *
+	 * Si la date n'est pas initialisée, retourne -1
+	 * @version 2.6 test si null
 	 * @return integer
 	 */
 	public function getTimestamp() {
-		return $this -> _date;
+		if (is_null($this -> _date)) {
+			$ret = -1;
+		} else {
+			$ret = $this -> _date -> getTimeStamp();
+		}
+		return $ret;
 	}
+
 	/** 1.1 : ajout
 	 * Renvoie un objet php DateTime
+	 * @since 1.1
+	 * @return DateTime
 	 */
 	public function getDate() {
-		$ret = new DateTime();
-		$ret -> setTimestamp($this -> _date);
-		return $ret;
+		return $this -> _date;
 	}
 
 	/**
@@ -101,7 +99,7 @@ class SG_Date extends SG_Objet {
 	 */
 	function toString() {
 		if (!is_null($this -> _date)) {
-			$ret = date('d/m/Y', $this -> _date);
+			$ret = $this -> _date -> format('d/m/Y');
 		} else {
 			$ret = '';
 		}
@@ -110,7 +108,6 @@ class SG_Date extends SG_Objet {
 
 	/**
 	 * Conversion valeur numérique
-	 *
 	 * @return float valeur numérique
 	 */
 	function toFloat() {
@@ -119,7 +116,6 @@ class SG_Date extends SG_Objet {
 
 	/**
 	 * Conversion valeur numérique
-	 *
 	 * @return integer valeur numérique
 	 */
 	function toInteger() {
@@ -127,191 +123,263 @@ class SG_Date extends SG_Objet {
 	}
 
 	/**
-	* Affichage
-	* @return string code HTML
-	*/
+	 * Affichage
+	 * @return string code HTML
+	 */
 	function afficherChamp() {
-		return '<span class="champ_Date">' . $this -> toHTML() -> texte . '</span>';
+		return '<span class="sg-date">' . $this -> toHTML() -> texte . '</span>';
 	}
 
-	/** 1.3.3 idRandom et init ; 2.0 stopPropagation
-	* Modification
-	*
-	* @param $pRefChamp référence du champ HTML
-	*
-	* @return string code HTML
-	*/
+	/**
+	 * Crée un champ HTML pour la modification
+	 * Un id Random estaffecté au champ et appel js 
+	 * @param $pRefChamp référence du champ HTML
+	 * @return string code HTML
+	 * @uses SynerGaia.initChampDate() SynerGaia.stopPropagation()
+	 */
 	function modifierChamp($pRefChamp = '') {
 		// Identifiant unique du tableau
-		$idChamp = SG_Champ::idRandom();
-		$ret = '<input id="' . $idChamp . '" class="champ_Date" type="text" name="' . $pRefChamp . '"';
+		$idChamp = SG_SynerGaia::idRandom();
+		$ret = '<input id="' . $idChamp . '" class="sg-date" type="text" name="' . $pRefChamp . '"';
 		$ret.= ' value="' . str_replace('"', '&quot;', $this -> toString()) . '" ondblclick="SynerGaia.stopPropagation(event);"/>';
 		$ret.= '<script>SynerGaia.initChampDate("#' . $idChamp . '")</script>';
 		return $ret;
 	}
-	/** 1.1 : ajout
-	* EstVide
-	*/
+
+	/**
+	 * EstVide
+	 * @since 1.1
+	 * @return SG_VraiFaux
+	 */
 	public function EstVide() {
 		$ret = new SG_VraiFaux($this -> _date === null);
 		return $ret;
 	}
-	/** 1.0.7 : ajout ; 1.3.1 cas null
-	* Annee
-	*/
+
+	/**
+	 * Annee
+	 * @since 1.0.7
+	 * @return SG_Nombre|SG_Rien
+	 */
 	public function Annee() {
 		if (is_null($this -> _date)) {
 			$ret = new SG_Rien();
 		} else {
-			$d = intval(date('Y', $this -> _date));
+			$d = intval($this -> _date -> format('Y'));
 			$ret = new SG_Nombre($d);
 		}
 		return $ret;
 	}
-	/** 1.0.7 : ajout
-	* Mois
-	*/
+
+	/**
+	 * Mois
+	 * @since 1.0.7
+	 * @return SG_Nombre|SG_Rien
+	 */
 	public function Mois() {
 		if (is_null($this -> _date)) {
 			$ret = new SG_Rien();
 		} else {
-			$d = intval(date('m', $this -> _date));
+			$d = intval($this -> _date -> format('m'));
 			$ret = new SG_Nombre($d);
 		}
 		return $ret;
 	}
-	/** 1.0.7 : ajout
-	* Jour
-	*/
+
+	/**
+	 * Jour
+	 * @since 1.0.7
+	 * @return SG_Nombre|SG_Rien
+	 */
 	public function Jour() {
 		if (is_null($this -> _date)) {
 			$ret = new SG_Rien();
 		} else {
-			$d = intval(date('d', $this -> _date));
+			$d = intval($this -> _date -> format('d'));
 			$ret = new SG_Nombre($d);
 		}
 		return $ret;
 	}
-	/** 1.1 : ajout
-	*/   
+
+	/**
+	 * Indique si la date en cours est supérieure strictement à la date passée en paramètre
+	 * @since 1.1
+	 * @param indéfini $pQuelqueChose valeur à comparer
+	 * @return SG_VraiFaux
+	 */   
     function SuperieurA ($pQuelqueChose = null) {
 		if ($pQuelqueChose === null) {
 			$date = new SG_Date(today);
 		} else {
 			$date = new SG_Date($pQuelqueChose);
 		}
-		$ret = new SG_VraiFaux( $this -> _date >= $date -> _date);
+		$ret = new SG_VraiFaux( $this -> _date > $date -> _date);
 		return $ret;
 	}
-	/** 1.1 : ajout
-	*/   
+
+	/**
+	 * cette date est-elle strictement inférieure à la date passée en paramètre
+	 * @since 1.1
+	 * @param indéfini $pQuelqueChose valeur à comparer
+	 * @return SG_VraiFaux
+	 */   
     function InferieurA ($pQuelqueChose = null) {
 		if ($pQuelqueChose === null) {
 			$date = new SG_Date(today);
 		} else {
 			$date = new SG_Date($pQuelqueChose);
 		}
-		$ret = new SG_VraiFaux( $this -> _date <= $date -> _date);
+		$ret = new SG_VraiFaux( $this -> _date < $date -> _date);
 		return $ret;
 	}
-	/** 1.1 : ajout
-	* AJouter ou retranche du temps
-	*/
-	public function Ajouter($pJours = 0, $pMois = 0, $pAnnees = 0) {
-		$intval = '';
-		$n = new SG_Nombre($pJours);
-		$n = $n -> toInteger();
-		if($n !== 0) {
-			$intval .= $n . ' days ';
-		}
-		$n = new SG_Nombre($pMois);
-		$n = $n -> toInteger();
-		if($n !== 0) {
-			if($intval !== '' and $n > 0) {
-				$intval .= '+';
+
+	/**
+	 * Ajouter ou retranche du temps à la date en cours (qui est modifiée)
+	 * @since 1.1
+	 * @version 2.6 alignement des pareamètres sur 
+	 * @param SG_Nombre|SG_Formule|integer Quantité à ajouter
+	 * @param SG_Texte|SG_Formule|string unté de la quantité
+	 * @return SG_Date la date mise à jour
+	 */
+	public function Ajouter($pQuantite = 0, $pUnite = 'j') {
+		$qte = new SG_Nombre($pQuantite);
+		$qte = $qte -> valeur;
+		$unite = substr(strtolower(SG_Texte::getTexte($pUnite)),0,2);
+		$dt = new SG_Date();
+		$dt -> _date = $this -> _date;
+		if ($dt -> _date != null and $qte !== 0) {
+			switch ($unite) {
+				case 'j':
+				case 'jo':
+				case 'd':
+				case 'da':
+					$q = $qte . 'D';
+					break;
+				case 'm':
+				case 'mo':
+					$q = $qte . 'M';
+					break;
+				case 'a':
+				case 'y':
+				case 'an':
+				case 'ye':
+					$q = $qte . 'Y';
+					break;
+				default:
+					$q = '';
+					break;
+				
 			}
-			$intval .= $n . ' months ';
+			$dt -> _date -> add(new DateInterval('P' . $q));
 		}
-		$n = new SG_Nombre($pAnnees);
-		$n = $n -> toInteger();
-		if($n !== 0) {
-			if($intval !== '' and $n > 0) {
-				$intval .= '+';
-			}
-			$intval .= $n . ' years ';
-		}
-		$date = new SG_Date($this -> getDate() -> add(DateInterval::createFromDateString($intval)));
-		return $date;
+		return $dt;
 	}
-	/** 1.1 new
-	*/
+
+	/**
+	 * Calcule l'âge actuel en année
+	 * @since 1.1
+	 * @version 2.6 parm
+	 * @param SG_Date|SG_Formule $dt une date pour le décès
+	 * @return SG_Nombre
+	 */
 	function Age() {
-		$now = new DateTime();
+		if (func_num_args() >= 1) {
+			$dt = new SG_Date(func_get_arg(0));
+			$now = $dt -> _date;
+		} else {
+			$now = new DateTime();
+		}
 		$interval = $now -> diff($this -> getDate());
 		return new SG_Nombre($interval -> y);
-	}	/** 1.1 new
-	*/
+	}
+	
+	/**
+	 * Indique si les deux dates sont identiques
+	 * @since 1.1
+	 * @param SG_Date $pAutreDate
+	 * @return SG_VraiFaux
+	 */
 	function Egale($pAutreDate = '') {
 		if ($pAutreDate === '') {
 			$date = SG_Rien::Aujourdhui();
 		} else {
 			$date = new SG_Date($pAutreDate);
 		}
-		if ($this -> _date === $date -> _date) {
-			$ret = new SG_VraiFaux(true);
+		if (! is_null($date -> _date)) {
+			if ($this -> _date === $date -> _date) {
+				$ret = new SG_VraiFaux(true);
+			} else {
+				$ret = new SG_VraiFaux(false);
+			}
 		} else {
-			$ret = new SG_VraiFaux(false);
+			$ret = new SG_VraiFaux(SG_VraiFaux::VRAIFAUX_INDEF);
 		}
 		return $ret;
 	}
-	/** 1.3.4 ajout
-	* Format string pour tri (aaaa/mm/jj)
-	**/
+
+	/**
+	 * Format string pour tri (aaaa/mm/jj)
+	 * @since 1.3.4
+	 * @return string
+	 **/
 	function AnMoisJour() {
 		$ret = new SG_Texte('');
 		if (is_null($this -> _date)) {
 			$ret = '';
 		} else {
-			$ret -> texte = date('Y', $this -> _date) . '/' . date('m', $this -> _date) . '/' . date('d', $this -> _date);
+			$ret -> texte = $this -> _date -> format('Y') . '/' . $this -> _date -> format('m') . '/' . $this -> _date -> format('d');
 		}
 		return $ret;
 	}
-	/** 2.1.1 ajout ; 2.3 correction days
+
+	/**
 	* calcule le nombre de jours écoulé avec la date passée en paramètre
-	* @param (SG_Date) $pDate : autre date pour l'intervalle
+	* @since 2.1.1
+	* @param SG_Date $pDate : autre date pour l'intervalle
+	* @return SG_Nombre
 	**/
 	function DureeDepuis($pDate = null) {
 		$ret = 0;
 		if ($pDate !== null) {
 			$d = new SG_Date($pDate);
-			$d = $d -> getDate();
-			$interval = $d -> diff($this -> getDate());
-			$ret = $interval -> days;
+			if (! is_null($this -> _date) and ! is_null($d -> date)) {
+				$interval = $d -> diff($this -> _date());
+				$ret = $interval -> days;
+			}
 		}
 		return new SG_Nombre($ret);
 	}
+
 	/** 2.1.1 ajout
-	* Indique si la date est de l'année en cours ou non
-	**/
+	 * Indique si la date est de l'année en cours ou non
+	 * @since 2.1.1
+	 * @return SG_VraiFaux
+	 **/
 	function CetteAnnee() {
 		return new SG_VraiFaux($this -> Annee() -> toString() === SG_Rien::Aujourdhui() -> Annee() -> toString());
 	}
-	/** 2.3 ajout
-	* Permet la concaténation de texte directement
-	* @param texte à concatener
-	* @return SG_Texte
-	**/
+
+	/**
+	 * Permet la concaténation de texte directement
+	 * @since 2.3
+	 * @param texte à concatener
+	 * @return SG_Texte
+	 **/
 	function Concatener() {
 		$args = func_get_args();
 		$ret = new SG_Texte($this -> toString());
 		$ret = call_user_func_array(array($ret,'Concatener'), $args);
 		return $ret;
 	}
-	/** 2.3 ajout
-	* enlève la quantité fournie à la date et heure
-	* @param (@Nombre) : quantité à soustraire
-	* @param (@Texte) : code unité (seuls les 2 1ers caractères sont utilisés) j, jo, d, da, m, mo, a, y, an, ye
-	*/
+
+	/**
+	 * enlève la quantité fournie à la date et heure
+	 * @since 2.3
+	 * @version 2.6 DateTime
+	 * @param (@Nombre) : quantité à soustraire
+	 * @param (@Texte) : code unité (seuls les 2 1ers caractères sont utilisés) j, jo, d, da, m, mo, a, y, an, ye
+	 * @return SG_Date Une SG_Date après modification
+	 */
 	function Soustraire($pQuantite = 0, $pUnite = 'j') {
 		$qte = new SG_Nombre($pQuantite);
 		$qte = $qte -> valeur;
@@ -324,24 +392,109 @@ class SG_Date extends SG_Objet {
 				case 'jo';
 				case 'd':
 				case 'da':
-					$qte = $qte * 86400; // nb secondes dans jour
+					$u = 'D'; // nb secondes dans jour
 					break;
 				case 'm':
 				case 'mo':
-					$qte = $qte * 2592000; // nb seconds dans 30 jours;
+					$u = 'M'; // nb seconds dans 30 jours;
 					break;
 				case 'a':
 				case 'y':
 				case 'an':
 				case 'ye':
-					$qte = $qte * 31536000; // nb seconds dans 365 jours;
-				break;
+					$u = 'Y'; // nb seconds dans 365 jours;
+					break;
 			}
-			$dt -> _date -= $qte;
+			$dt -> _date -> sub(new DateInterval('P' . $qte . $u));
 		}
 		return $dt;
 	}
-	// 2.1.1. complément de classe créée par compilation
+
+	/**
+	 * Retourne @Vrai si la date est dans l'intervalle (bornes incluses)
+	 * @since 2.4
+	 * @param (date ou dateheure) borne inférieure (défaut 0)
+	 * @param (date ou dateheure) borne supérieure (défaut 0)
+	 * @return @VraiFaux
+	 */
+	public function Entre($pInf = 0, $pSup = 0) {
+		/** récupération des paramètres */
+		if (getTypeSG($pInf) === '@Formule') {
+			$inf = $pInf -> calculer();
+		} else {
+			$inf = $pInf;
+		}
+		if (getTypeSG($pSup) === '@Formule') {
+			$sup = $pSup -> calculer();
+		} else {
+			$sup = $pSup;
+		}
+		/** calcul des limites */
+		if (getTypeSG($inf) === '@Periode' and $sup === 0) {
+			$sup = $inf -> Fin();
+			$inf = $inf -> Debut();
+		} else {
+			if($inf === 0) {
+				$inf = SG_Rien::Aujourdhui();
+			} else {
+				$inf = new SG_Date($inf);
+			}
+			if($sup === 0) {
+				$sup = SG_Rien::Aujourdhui();
+			} else {
+				$sup = new SG_Date($sup);
+			}
+		}
+		/** comparaison */
+		if (is_null($sup -> _date) or is_null($inf -> _date)) {
+			$ret = new SG_Erreur('0212',"0212");
+		} elseif ($inf -> SuperieurA($sup) -> estVrai()) {
+			$ret = new SG_Erreur('0214',"0214");
+		} else {
+			try {
+				$ret = !($this -> _date < $inf -> _date or $this -> _date > $sup -> _date);
+				$ret = new SG_VraiFaux($ret);
+			} catch (Exception $e) {
+				$ret = new SG_Erreur('0213',"0213");
+			}
+		}
+		/** retour */
+		return $ret;
+	}
+
+	/**
+	 * Calcule le premier jour de la semaine contenant le jour (commence au lundi)
+	 * @since 2.5
+	 * @return SG_Date : le jour du début de semaine (éventuellement année précédente)
+	 **/
+	public function Lundi() {
+		$ret = new SG_Date();
+		$d = $this -> _date -> format('w');
+		if ($d === '0') {
+			$ret = $this -> Soustraire(6);
+		} else {
+			$ret = $this -> Soustraire(intval($d) - 1);
+		}
+		return $ret;
+	}
+
+	/**
+	 * Format string pour tri (aaaa/mm)
+	 * 
+	 * @since 2.7
+	 * @return string
+	 **/
+	function AnMois() {
+		$ret = new SG_Texte('');
+		if (is_null($this -> _date)) {
+			$ret = '';
+		} else {
+			$ret -> texte = $this -> _date -> format('Y') . '/' . $this -> _date -> format('m');
+		}
+		return $ret;
+	}
+
+	/** 2.1.1. complément de classe créée par compilation */
 	use SG_Date_trait;
 }
 ?>
