a91c33ec650247e891785f49fdd384184a9c0eea
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 15:12:08 +0200

    SynerGaia 2.6

--- lib/sg_Personne.php
+++ lib/sg_Personne.php
@@ -1,31 +1,40 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.1.1 (see AUTHORS file)
-* Classe SynerGaia de gestion d'une personne
-*/
-// 2.1.1 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+<?php
+/** SYNERGAIA fichier pour le traitement de l'objet @Personne */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_Personne_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_Personne_trait.php';
 } else {
+	/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur */
 	trait SG_Personne_trait{};
 }
+
+/**
+* Classe SynerGaia de gestion d'une personne
+* @since 1.1
+* @version 2.1.1
+*/
 class SG_Personne extends SG_Document {
-	// Type SynerGaia
+	/** string Type SynerGaia '@Personne' */
 	const TYPESG = '@Personne';
-	public $typeSG = self::TYPESG;
-	
-	// code base par défaut
+
+	/** string code base par défaut*/
 	const CODEBASE = 'synergaia_personnes';
 
+	/** string Type SynerGaia */
+	public $typeSG = self::TYPESG;
+
+	/** string Code de la personne */
 	public $code;
 	
-	/** 1.1
-	* Construction de l'objet
-	*
-	* @param string $pCode code éventuel de la personne (permet la différenciation des homonymes)
-	* @param indefini $pTableau tableau éventuel des propriétés du document CouchDB ou SG_DocumentCouchDB
-	* @param string $pNom nom de famille
-	* @param string $pPrenom prénom usuel 
-	*/
+	/**
+	 * Construction de l'objet
+	 * @since 1.1
+	 * @param string $pCode code éventuel de la personne (permet la différenciation des homonymes)
+	 * @param indefini $pTableau tableau éventuel des propriétés du document CouchDB ou SG_DocumentCouchDB
+	 * @param string $pNom nom de famille
+	 * @param string $pPrenom prénom usuel
+	 */
 	public function __construct($pCode = '', $pTableau= null, $pNom = '', $pPrenom = '') {
 		$tmpCode = new SG_Texte($pCode);
 		$base = self::CODEBASE;
@@ -44,13 +53,23 @@ class SG_Personne extends SG_Document {
 			$this -> setValeur('@Prenom', $prenom -> texte);
 		}
 	}
-	/** 1.1
-	*/
+
+	/**
+	 * Retourne le titre de la personne (Nom Prénom)
+	 * 
+	 * @since 1.1
+	 * @version 2.6 return SG_Texte
+	 * @return SG_Texte
+	 */
 	public function Titre() {
-		return $this -> getValeur('@Nom') . ' ' . $this -> getValeur('@Prenom');
+		return new SG_Texte($this -> getValeur('@Nom') . ' ' . $this -> getValeur('@Prenom'));
 	}
-	/** 1.1
-	*/
+
+	/**
+	 * Retourne l'âge de la personne
+	 * @since 1.1
+	 * @return SG_Nombre
+	 */
 	public function Age() {
 		$ret = new SG_VraiFaux(false);
 		$dt = $this -> getValeur('@NaissanceDate', false);
@@ -59,6 +78,7 @@ class SG_Personne extends SG_Document {
 		}
 		return $ret;
 	}
+
 	// 2.1.1. complément de classe créée par compilation
 	use SG_Personne_trait;
 }
