7d581e3936cc89f1685a7908f419135ba939448a
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 15:55:46 +0200

    SynerGaia 2.6

--- lib/sg_VraiFaux.php
+++ lib/sg_VraiFaux.php
@@ -1,54 +1,49 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.3 (see AUTHORS file)
-* Classe SynerGaia de traitement des booléens
-*
-* Chaque booléen peut en réalité avoir un des 3 états possibles :
-* vrai, faux ou indéfini (par exemple dans le cas d'une erreur de calcul)
-*/
-// 2.3 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+<?php
+/** fichier contenant les classes de gestion des éléments booléens @VraiFaux */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_VraiFaux_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_VraiFaux_trait.php';
 } else {
+	/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+	 * par défaut trait vide */
 	trait SG_VraiFaux_trait{};
 }
+
+/**
+ * Classe SynerGaia de traitement des booléens
+ * Chaque booléen peut avoir un des 3 états possibles :
+ * vrai, faux ou indéfini (par exemple dans le cas d'une erreur de calcul) (vrai, faux et indéfini)
+ * @since 1.0
+ * @version 2.4
+ */
 class SG_VraiFaux extends SG_Objet {
-	// Type SynerGaia
+	/** string Type SynerGaia '@VraiFaux' */
 	const TYPESG = '@VraiFaux';
+	/** string Type SynerGaia */
 	public $typeSG = self::TYPESG;
 	
-	// Valeur pour "VRAI"
+	/** string Valeur pour "VRAI" */
 	const VRAIFAUX_VRAI = 1;
-	/**
-	 * Valeur pour "FAUX"
-	 */
+	/** string Valeur pour "FAUX" */
 	const VRAIFAUX_FAUX = -1;
-	/**
-	 * Valeur pour "INDEFINI"
-	 */
+	/** string Valeur pour "INDEFINI" */
 	const VRAIFAUX_INDEF = 0;
 
-	/**
-	 * Texte pour "VRAI"
-	 */
+	/** string Texte pour "VRAI" */
 	const VRAIFAUX_TEXTE_VRAI = 'oui';
-	/**
-	 * Texte pour "FAUX"
-	 */
+	/** string Texte pour "FAUX" */
 	const VRAIFAUX_TEXTE_FAUX = 'non';
-	/**
-	 * Texte pour "INDEFINI"
-	 */
+	/** string Texte pour "INDEFINI" */
 	const VRAIFAUX_TEXTE_INDEF = 'indéfini';
 
-	/**
-	 * Valeur interne de l'objet
-	 */
+	/** integer Valeur interne de l'objet (1 vrai, 0 indéfini, -1 faux)*/
 	public $valeur = 0;
 
-	/** 1.3.2 si $pQuelqueChose type @Document : définit l'existence physique du document, @Collection = non vide
+	/**
 	 * Construction de l'objet
-	 *
-	 * @param indéfini $pQuelqueChose valeur à partir de laquelle le SG_VraiFaux est créé
+	 * @version 1.3.2 si $pQuelqueChose type @Document : définit l'existence physique du document, @Collection = non vide
+	 * @param any $pQuelqueChose valeur à partir de laquelle le SG_VraiFaux est créé
 	 * @level 0 sauf si les paramètres sont des objets
 	 */
 	function __construct($pQuelqueChose = null) {
@@ -107,7 +102,7 @@ class SG_VraiFaux extends SG_Objet {
 						$this -> valeur = self::VRAIFAUX_FAUX;
 					}
 				} elseif($type === '@Fichier') {
-					if (isset($v->proprietes[$v->reference]['data'])) {
+					if (isset($v->proprietes[$v -> reference]['data'])) {
 						$this -> valeur = self::VRAIFAUX_VRAI;
 					} else {
 						$this -> valeur = self::VRAIFAUX_FAUX;
@@ -130,7 +125,6 @@ class SG_VraiFaux extends SG_Objet {
 
 	/**
 	 * Conversion en chaine de caractères
-	 *
 	 * @return string "vrai", "faux" ou "indéfini"
 	 */
 	function toString() {
@@ -148,9 +142,9 @@ class SG_VraiFaux extends SG_Objet {
 		return $texte;
 	}
 
-	/** 2.1.1
+	/**
 	 * Conversion en code HTML
-	 *
+	 * @version 2.1.1
 	 * @return string code HTML
 	 */
 	function toHTML() {
@@ -159,7 +153,7 @@ class SG_VraiFaux extends SG_Objet {
 
 	/**
 	 * Affichage
-	 *
+	 * 
 	 * @return string code HTML
 	 */
 	function afficherChamp() {
@@ -169,8 +163,7 @@ class SG_VraiFaux extends SG_Objet {
 	/**
 	 * Modification
 	 *
-	 * @param $pRefChamp référence du champ HTML
-	 *
+	 * @param string $pRefChamp référence du champ HTML
 	 * @return string code HTML
 	 */
 	function modifierChamp($pRefChamp = '') {
@@ -201,9 +194,12 @@ class SG_VraiFaux extends SG_Objet {
 		return $ret;
 	}
 
-	/** 1.3.1 paramètres
-	 * Conversion en SG_Texte
-	 * @param (SG_Texte) valeur si vrai (sinon 'vrai')
+	/**
+	 * Traduction en SG_Texte (selon les constantes VRAIFAUX_TEXTE_??? ou les paramètres)
+	 * @version 1.3.1 paramètres
+	 * @param SG_Texte $pVrai libellé si vrai
+	 * @param SG_Texte $pFaux libellé si faux
+	 * @param SG_Texte $pIndefini libellé si indéfini
 	 * @return SG_Texte "vrai", "faux" ou "indéfini"
 	 */
 	function EnTexte($pVrai = self::VRAIFAUX_TEXTE_VRAI, $pFaux = self::VRAIFAUX_TEXTE_FAUX, $pIndefini = self::VRAIFAUX_TEXTE_INDEF) {
@@ -228,11 +224,12 @@ class SG_VraiFaux extends SG_Objet {
 	}
 
 	/**
-	 * Conversion en booléen
-	 *
+	 * Conversion en booléen (indéfini est traduit en false sauf paramètre)
+	 * @version 2.6 paramètre
+	 * @param boolean $pIndefini valeur si indéfini (par défaut false)
 	 * @return boolean
 	 */
-	function estVrai() {
+	function estVrai($pIndefini = false) {
 		$ret = false;
 		switch($this->valeur) {
 			case self::VRAIFAUX_VRAI :
@@ -242,7 +239,7 @@ class SG_VraiFaux extends SG_Objet {
 				$ret = false;
 				break;
 			default :
-				$ret = false;
+				$ret = $pIndefini;
 		}
 		return $ret;
 	}
@@ -273,11 +270,13 @@ class SG_VraiFaux extends SG_Objet {
 
 	/**
 	 * Inversion du SG_VraiFaux
-	 *
+	 * 
+	 * @version 2.4 parm
+	 * @param boolean|SG_VraiFaux|SG_Formule $pSiIndefini : retourne la valeur à donner si indéfini
 	 * @return SG_VraiFaux
 	 */
-	function Non() {
-		switch($this->valeur) {
+	function Non($pSiIndefini = null) {
+		switch($this -> valeur) {
 			case self::VRAIFAUX_VRAI :
 				$ret = new SG_VraiFaux(false);
 				break;
@@ -285,16 +284,24 @@ class SG_VraiFaux extends SG_Objet {
 				$ret = new SG_VraiFaux(true);
 				break;
 			default :
-				$ret = new SG_VraiFaux();
+				if ($pSiIndefini !== null) {
+					if (getTypeSG($pSiIndefini) === '@Formule') {
+						$ret = $pSiIndefini -> calculer();
+					} else {
+						$ret = $pSiIndefini;
+					}
+				} else {
+					$ret = new SG_VraiFaux();
+				}
 				break;
 		}
 		return $ret;
 	}
 
-	/** 1.1
+	/**
 	 * Ou logique
-	 *
-	 * @param indéfini $pQuelqueChose valeur avec laquelle effectuer le "ou"
+	 * @since 1.1
+	 * @param boolean|SG_VraiFaux|SG_Formule $pQuelqueChose valeur avec laquelle effectuer le "ou"
 	 *
 	 * @return SG_VraiFaux
 	 */
@@ -338,10 +345,10 @@ class SG_VraiFaux extends SG_Objet {
 		return $ret;
 	}
 
-	/** 1.1
+	/**
 	 * Et logique ($pQuelquechose n'est calculé que si nécessaire)
-	 *
-	 * @param indéfini $pQuelqueChose valeur avec laquelle effectuer le "et"
+	 * @since 1.1
+	 * @param boolean|SG_VraiFaux|SG_Formule $pQuelqueChose valeur avec laquelle effectuer le "et"
 	 *
 	 * @return SG_VraiFaux
 	 */
@@ -384,17 +391,15 @@ class SG_VraiFaux extends SG_Objet {
 		}
 		return $ret;
 	}
-	/** 1.1
+
+	/**
+	 * Attribue un nom d'icône en fonction de la valeur
+	 * @since 1.1 ajout
+	 * @param string $pVrai = accept.png
+	 * @param string $pFaux = cancel.png
+	 * @param string $pIndef ''
+	 * @return @Icone
 	 */
-	function Tracer($pMsg = '') {
-		$msg = new SG_Texte($pMsg);
-		echo '<b>' . $msg -> texte . ' : (' . $this -> typeSG . ') </b>' . $this -> toString();
-		return $this;
-	}
-	/** 1.1 ajout
-	* vrai = accept.png, faux = cancel.png
-	* @return @Icone
-	*/
 	function Icone($pVrai = 'accept.png', $pFaux = 'cancel.png', $pIndef = '') {
 		$icone = '';
 		switch($this->valeur) {
@@ -409,19 +414,23 @@ class SG_VraiFaux extends SG_Objet {
 		}
 		return $icone;
 	}
-	/** 1.3.1 ajout
-	/ Retourne la valeur booleénne d'une expression. Indéfini est retourné false
-	* @param (any) valeur à tester
-	* @return (boolean) valeur boléenne
-	**/
+
+	/**
+	 * Retourne la valeur booleénne d'une expression. Indéfini est retourné false
+	 * @since 1.3.1 ajout
+	 * @param any $pValeur valeur à tester
+	 * @return (boolean) valeur boléenne
+	 **/
 	static function getBooleen ($pValeur = '') {
 		$val = new SG_VraiFaux($pValeur);
 		return $val -> estVrai();
 	}
-	/** 1.3.2 ajout
-	* met à jour la valeur selon un booleen vrai (sinon indefini)
-	* @param ($pBooleen) valeur à mettre
-	**/
+
+	/**
+	 * met à jour la valeur selon un booleen vrai (sinon indefini)
+	 * @since 1.3.2 ajout
+	 * @param boolean $pBooleen valeur à mettre
+	 **/
 	function setBooleen($pBooleen = SG_VraiFaux::VRAIFAUX_INDEF) {
 		if ($pBooleen === true) {
 			$this -> valeur = SG_VraiFaux::VRAIFAUX_VRAI;
@@ -431,6 +440,7 @@ class SG_VraiFaux extends SG_Objet {
 			$this -> valeur = SG_VraiFaux::VRAIFAUX_INDEF;
 		}
 	}
+
 	// 2.3 complément de classe créée par compilation
 	use SG_VraiFaux_trait;
 }
