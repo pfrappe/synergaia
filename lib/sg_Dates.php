fc6d8c156d0458eb805c33f34662249dcc2f6532
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 14:24:13 +0200

    SynerGaia 2.6

--- lib/sg_Dates.php
+++ lib/sg_Dates.php
@@ -1,21 +1,29 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.1.1 (see AUTHORS file)
-* SG_Dates : Classe de traitement des dates multiples
-*/
-// 2.1.1 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+<?php
+/** SYNERGAIA fichier pour le traitement de liste de dates */
+ defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
+
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_Dates_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_Dates_trait.php';
 } else {
+	/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur 
+	 * @since 2.1.1
+	 */
 	trait SG_Dates_trait{};
 }
+
+/**
+ * SG_Dates : Classe de traitement de liste de dates
+ * @version 2.6.0
+ */
 class SG_Dates extends SG_Objet {
-	// Type SynerGaia
+	/** string Type SynerGaia '@Dates' */
 	const TYPESG = '@Dates';
-	// Type SynerGaia de l'objet
+	/** string Type SynerGaia de l'objet */
 	public $typeSG = self::TYPESG;
 
 	/**
-	* Tableau des dates (@Date)
+	* array Tableau des dates SG_Date
 	*/
 	public $elements = array();
 
@@ -39,33 +47,41 @@ class SG_Dates extends SG_Objet {
 			}
 		}
 	}
+
 	/**
-	**/
+	 * met la liste en chaine de caratcères
+	 * return string
+	 */
 	function toString() {
 		$tmp = array();
 		foreach($this -> elements as $dt) {
 			$tmp[] = $dt -> toString();
 		}
 		return implode(', ', $tmp);
-	} 
+	}
+
 	/**
-	* Modifier correctement un champ avec un picker de dates multiples
-	* @param (string) $pRefChamp référence html du champ
-	**/
+	 * Calcule le code html pour modifier correctement un champ avec un picker de dates multiples
+	 * @param string $pRefChamp référence html du champ
+	 * @return string code html
+	 * @uses SynerGaia.initChampDates()
+	 */
 	function modifierChamp($pRefChamp = '') {
 		$txt = $this -> toString();
-		$idChamp = SG_Champ::idRandom();
+		$idChamp = SG_SynerGaia::idRandom();
 		$ret = '<input id="' . $idChamp . '" class="champ_Dates hasDatePicker" type="text" name="' . $pRefChamp . '" value="' . str_replace('"', '&quot;', $txt) . '"/>';
 		$ret.= '<script>SynerGaia.initChampDates("#' . $idChamp . '", "' . $txt . '")</script>';
 		return $ret;
 	}
+
 	/**
-	* Afficher la liste des dates
-	* @return string code HTML
-	**/
+	 * Calcule le code html pour afficher la liste des dates comme un champ
+	 * @return string code HTML
+	 */
 	function afficherChamp() {
 		return '<span class="champ_Dates">' . $this -> toHTML() -> texte . '</span>';
 	}
+
 	// 2.1.1. complément de classe créée par compilation
 	use SG_Dates_trait;
 }
