8ac746afabafa1e5570b8ee685879412418f1239
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 12:36:54 +0200

    Mise en forme pour phpDocumentor

--- lib/sg_Adresse.php
+++ lib/sg_Adresse.php
@@ -1,29 +1,44 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.1.1 (see AUTHORS file)
-* SG_Adresse : Classe SynerGaia de gestion d'une adresse géographique
-*/
+<?php
+/** SynerGaïa Fichier contenant la classe SG_Adresse */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
 // 2.1.1 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_Adresse_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_Adresse_trait.php';
 } else {
+	/** trait vide par défaut */
 	trait SG_Adresse_trait{};
 }
+
+/** SynerGaia
+ * SG_Adresse : Classe SynerGaia de gestion d'une adresse géographique
+ * @since 1.1
+ */
 class SG_Adresse extends SG_ObjetComposite {
-	// Type SynerGaia
+	/** string Type SynerGaia */
 	const TYPESG = '@Adresse';
+	/** string Type SynerGaia */
 	public $typeSG = self::TYPESG;	
 
-	public $classeCSS = 'adresse';
+	/** string classe css associée */
+	public $classeCSS = 'sg-adresse';
 
 	/** 1.1 ajout
 	* Construction de l'objet
+	* @since 1.1
+	* @param SG_Formule|any $pQuelqueChose
+	* @param string $pUUId
 	*/
 	function __construct($pQuelqueChose = null, $pUUId = '') {
 		$this -> initObjetComposite($pQuelqueChose, $pUUId);
 		$this -> champs = array('@Numero' => null, '@Rue' => null, '@Complement' => null, '@BoitePostale' => null, '@CodePostal' => null, '@Ville' => null, '@Pays' => null);
-	}	
-	/** 1.2 ajout
-	*/
+	}
+
+	/** 
+	 * affichage du champ Adresse
+	 * @since 1.2
+	 * @return SG_HTML
+	 */
 	function afficherChamp() {
 		$ville = $this -> getValeurPropriete('@Ville', '');
 		if(getTypeSG($ville) === '@Ville') {
@@ -31,15 +46,24 @@ class SG_Adresse extends SG_ObjetComposite {
 		} else {
 			$ret = '';
 		}
-		return $ret . $this -> Afficher();
+		$ret = new SG_HTML($ret);
+		return $ret -> Concatener($this -> Afficher());
 	}
-	/**1.2 ajout
-	*/
+
+	/**
+	 * transforme l'objet en chaine de caractères
+	 * @since 1.2
+	 * @return string
+	 */
 	function toString() {
 		return implode(', ', $this -> tableau());
 	}
-	/** 1.2 ajout
-	*/
+
+	/**
+	 * transforme l'adresse en tableau
+	 * @since 1.2
+	 * return array
+	 */
 	function tableau() {
 		$ret = array($this -> getValeur('@Numero', '') . ' ' . $this -> getValeur('@Rue', ''));
 		$champ = $this -> getValeur('@Complement', '');
@@ -64,13 +88,21 @@ class SG_Adresse extends SG_ObjetComposite {
 		}
 		return $ret;
 	}
-	/**1.2 ajout
-	*/
+
+	/**
+	 * Transforme l'objet en texte HTML
+	 * @since 1.2
+	 * @return string
+	 */
 	function toHTML() {
-		return '<span class="adresse">' . implode('<br>', $this -> tableau()) . '</span>';
+		return '<span class="sg-composite">' . implode('<br>', $this -> tableau()) . '</span>';
 	}
-	/** 1.2 : ajout
-	*/
+
+	/**
+	 * compose la ligne code postal + ville
+	 * @since 1.2
+	 * @return string
+	 */
 	function ligneVille() {
 		$cedex = $this -> getValeur('@Cedex', '');
 		if ($cedex !== '') {
@@ -90,6 +122,7 @@ class SG_Adresse extends SG_ObjetComposite {
 		}
 		return $ret;
 	}
+
 	// 2.1.1. complément de classe créée par compilation
 	use SG_Adresse_trait;
 }
