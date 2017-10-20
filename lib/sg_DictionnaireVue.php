da926b3493464859050057ce7fd4c6428c8a27f6
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 14:29:04 +0200

    SynerGaia 2.6

--- lib/sg_DictionnaireVue.php
+++ lib/sg_DictionnaireVue.php
@@ -1,24 +1,30 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 1.1 (see AUTHORS file)
- * SG_DictionnaireBase : Classe de gestion d'une base de documents
+<?php
+/** SynerGaia fichier de gestion de l'objet @DictionnaireVue */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
+/**
+ * SG_DictionnaireVue : Classe de gestion d'une vue d'une base de documents
+ * @since 1.1
  */
 class SG_DictionnaireVue extends SG_Document {
-	// Type SynerGaia
+	/** string Type SynerGaia '@DictionnaireVue' */
 	const TYPESG = '@DictionnaireVue';
+	/** string Type SynerGaia */
 	public $typeSG = self::TYPESG;
 	
-	//code de la base
+	/** string code de la base de stockage */
 	const CODEBASE = 'synergaia_vues';
 
-	// Code de l'objet du dictionnaire
+	/** string Code de l'objet du dictionnaire */
 	public $code;
 
-	/** 1.1
-	* Construction de la vue
-	*
-	* @param string $pCodeVue code de l'objet demandé
-	* @param array tableau éventuel des propriétés
-	*/
+	/**
+	 * Construction de la vue
+	 * 
+	 * @since 1.1
+	 * @param string $pCodeVue code de l'objet demandé
+	 * @param array tableau éventuel des propriétés
+	 */
 	public function __construct($pCodeVue = '', $pTableau = '') {
 		$tmpCode = new SG_Texte($pCodeVue);
 		$base = self::CODEBASE;
