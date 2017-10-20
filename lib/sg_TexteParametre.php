0742f3763259174852300592ca52257c1d9aff0b
Pierre Frappé <pfrappe@gmail.com>
2017-10-20 15:49:54 +0200

    SynerGaia 2.6

--- lib/sg_TexteParametre.php
+++ lib/sg_TexteParametre.php
@@ -1,36 +1,47 @@
-<?php defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
-/** SynerGaia 2.1.1 (see AUTHORS file)
-* Classe SynerGaia de gestion d'un texte riche paramétrable
-*/
-// 2.1.1 Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur
+<?php
+/** SynerGaia fichier pour la gestion de l'objet @TexteParametre */
+defined("SYNERGAIA_PATH_TO_ROOT") or die('403.14 - Directory listing denied.');
+
 if (file_exists(SYNERGAIA_PATH_TO_APPLI . '/var/SG_TexteParametre_trait.php')) {
 	include_once SYNERGAIA_PATH_TO_APPLI . '/var/SG_TexteParametre_trait.php';
 } else {
+	/** Pour ajouter les méthodes et propriétés spécifiques de l'application créées par le compilateur */
 	trait SG_TexteParametre_trait{};
 }
+
+/**
+* Classe SynerGaia de gestion d'un texte riche paramétrable
+* @version 2.6 
+*/
 class SG_TexteParametre extends SG_TexteRiche {
-	// Type SynerGaia
+	/** string Type SynerGaia '@TexteParametre' */
 	const TYPESG = '@TexteParametre';
+	/** string Délimiteur de contenu actif (début) */
+	const DELIMITEUR_CONTENU_ACTIF_DEBUT = '{#';
+
+	/** string Délimiteur de contenu actif (fin) */
+	const DELIMITEUR_CONTENU_ACTIF_FIN = '#}';
+	/** string Type SynerGaia */
 	public $typeSG = self::TYPESG;
 	
-	// Délimiteur de contenu actif (début)
-	const DELIMITEUR_CONTENU_ACTIF_DEBUT = '{#';
-	
-	// Délimiteur de contenu actif (fin)
-	const DELIMITEUR_CONTENU_ACTIF_FIN = '#}';
 
-	/** 1.3.1 @param, return @HTML, strip_tags ; 2.1 si tableau ou erreur, suppression récursivité, param 2
-	* Conversion en code HTML
-	* @param (boolean ou @VraiFaux) $pCalculer : Calculer les formules intégrées (par défaut : oui)
-	* @param (SG_Document ou dérivé) $pDocument : document sur lequel s'appliqueront les formules
-	* @return string code HTML
-	*/
+	/**
+	 * Conversion en code HTML pour l'affichage sur le navigateur
+	 * C'est un peu plus que la traduction du SG_TexteRiche car, au passage,
+	 * on effectue la traduction des clauses entre délimiteurs {#...#}
+	 * 
+	 * @version 2.6 corrections : erreur si $calculer != true ; test objet résultat
+	 * @param boolean|SG_VraiFaux $pCalculer : Calculer les formules intégrées (par défaut : oui)
+	 * @param SG_Document $pDocument document sur lequel s'appliqueront les formules
+	 * @return string code HTML
+	 */
 	function Afficher($pCalculer = true, $pDocument = null) {
-		$calculer = new SG_VraiFaux($pCalculer);
-		$calculer = $calculer -> estVrai();
+		$calculer = SG_VraiFaux::getBooleen($pCalculer);
+		//$calculer = $calculer -> estVrai();
 		$texte = $this -> texte;
 		// afficher aussi comme texte riche
 		$texte = $this -> traduireLesURLInternes($texte);
+		$newtexte = $texte;
 		// traiter les formules
 		if ($calculer === true) {
 			if ($pDocument !== null) {
@@ -46,9 +57,7 @@ class SG_TexteParametre extends SG_TexteRiche {
 			}
 			$formule = new SG_Formule('', $docencours );
 			$ipos = strpos($texte, self::DELIMITEUR_CONTENU_ACTIF_DEBUT, 0);
-			if($ipos === false) {
-				$newtexte = $texte;
-			} else {
+			if($ipos !== false) {
 				$ifin = 0;
 				$newtexte = substr($texte,0,$ipos);
 				//boucle sur les balises de formules SynerGaia
@@ -62,7 +71,10 @@ class SG_TexteParametre extends SG_TexteRiche {
 							$resultat = $resultat[0];
 						}
 						if (is_object($resultat)) {
-							$resultat = $resultat -> toString();
+							$resultat = $resultat -> toHTML();
+							if(is_object($resultat)) {
+								$resultat = $resultat -> texte;
+							}
 						}
 						// ajuste la position de la fin
 						$newtexte.= $resultat;
@@ -77,9 +89,10 @@ class SG_TexteParametre extends SG_TexteRiche {
 				}
 			}
 		}
-		$ret = new SG_HTML('<richtext class="champ_TexteRiche">' . $newtexte . '</richtext>');
+		$ret = new SG_HTML('<richtext class="sg-richtext">' . $newtexte . '</richtext>');
 		return $ret;
 	}
+
 	// 2.1.1. complément de classe créée par compilation
 	use SG_TexteParametre_trait;
 }
